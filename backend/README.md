5f06b9d509e936ba0ea8080252db155baa4794e6
Alaric006 <alaricjenkins@outlook.com>
2025-10-28 21:47:11 -0400

    Created Python Microservices
    Created Python Microservices functionality, added test function in
    express backend to test that connection to microservices works properly.

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+# Overview
+Backend folder for our data analytics website
+
+# How To
+
+## Running Python Microservices
+
+Our microservices uses the flask python library. Make sure you already have python installed. Next, install flask:  
+``pip install flask``
+
+To run start the server, run:  
+``flask --app microservices run -p 5000``
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const fs = require('node:fs')
 const app = express()
 const hostname = '127.0.0.1'
 const port = 3000
+const microservices_hostname = "http://127.0.0.1:5000"
 
 app.get('/', (req, res) => {
 	res.send('Goodbye!');
@@ -23,9 +24,19 @@ app.get('/echo/:msg', (req, res) => {
 	res.send(req.params.msg);
 })
 
+app.get("/test-microservices", (req, res) => {
+	fetch(`${microservices_hostname}/test`)
+	.then( (response) => {
+		return response.text()
+	})
+	.then(data => res.send(data))
+	.catch( (err) => {
+		console.error(err)
+		res.status(500).send("Error fetching from microservices")
+	})
+})
+
 
 app.listen(port, hostname, () => {
 	console.log(`App listenting on ${hostname}:${port}`);
 });
-
-

9a54b3d39143edb81bd6f86741491fa5b12888b2
Alaric006 <alaricjenkins@outlook.com>
2025-11-04 19:29:39 -0500

    Added the basics of a homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 import Test from "./Test.jsx"
+import Home from "./Home.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/test/:text" element={<Test />} />
       </Routes>
     </BrowserRouter>

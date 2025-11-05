8d9f7999d9ac516e447ad25297e17cf9357763a8
Alaric006 <alaricjenkins@outlook.com>
2025-11-04 19:42:52 -0500

    Added useState in HomePage
    Demonstrated using React's useState.

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,16 @@
 import './Home.css'
+import {useState } from 'react'
 
 function Home() {
+    /* function setCount(newCounter) {
+        counter = newCount
+        // Does other stuff (save to some storage so that it isn't refreshed when we reload the page)
+    } */
+    let [counter, setCounter] = useState(1)
     return (
         <>
-            <h1> Welcome to the Home Page! </h1>
+            <h1> Welcome to the Home Page! { counter } </h1>
+            <button onClick={() => {setCounter(counter + 1)}}> Click me! </button>
         </>
     )
 }

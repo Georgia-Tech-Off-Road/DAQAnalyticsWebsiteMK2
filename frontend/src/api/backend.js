1cf07c4baa0b54294d864d2d180be30ced30a97d
Alaric Jenkins <alaricjenkins@outlook.com>
2026-01-12 14:00:13 -0500

    Refactored download function
    Refactored download function to api/backend.js. Added downloading
    capability to DatasetExplorer.jsx

--- frontend/src/api/backend.js
+++ frontend/src/api/backend.js
@@ -6,7 +6,14 @@ const GET_VEHICLES_URL = `${API_BASE}/vehicles`
 // Datasets
 const GET_DATASETS_URL = `${API_BASE}/datasets/`
 const UPLOAD_DATASET_URL = `${API_BASE}/datasets/upload`
+const GET_DATA_URL = `${API_BASE}/datasets/data/`
 
+
+async function getDatasetByID(id) {
+	const res = await fetch(`${GET_DATASETS_URL}${id}`)
+	if (!res.ok) throw new Error(`Failed to fetch dataset with ${id}`)
+	return res.json();
+}
 export const api = {
 // Vehicles
 	async getVehicles() {
@@ -22,11 +29,7 @@ export const api = {
 		return datasets
 	},
 
-	async getDatasetByID(id) {
-		const res = await fetch(`${GET_DATASETS_URL}${id}`)
-		if (!res.ok) throw new Error(`Failed to fetch dataset with ${id}`)
-		return res.json();
-	},
+	getDatasetByID,
 
 	async uploadDataset(form_data) {
 		const res = await fetch(UPLOAD_DATASET_URL, {
@@ -37,4 +40,29 @@ export const api = {
 
 		return res.json()
 	},
+
+	async downloadDataset(id) {
+		// TODO: Make it so that datafile and dataset are fetched concurrently
+		const res = await fetch(GET_DATA_URL)
+		const dataset = await getDatasetByID(id)
+		
+		if (!res.ok) {
+			throw new Error(`Failed to retrieve data file corresponding to the dataset with id: ${id}`)
+		}
+
+		const blob = await res.blob()
+
+		const url = window.URL.createObjectURL(blob)
+		const a = document.createElement('a')
+		a.href = url
+		a.download = dataset.title
+		document.body.appendChild(a)
+		a.click()
+
+		// Cleanup
+		window.URL.revokeObjectURL(url)
+		document.body.removeChild(a)
+	}
 }
+
+

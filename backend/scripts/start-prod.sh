4383ffd617609ece830e84fcb5aebfd7c6779979
Alaric Jenkins <alaricjenkins@outlook.com>
2026-02-27 11:41:57 -0500

    Moved nginx from running in ec2 user process

--- backend/scripts/start-prod.sh
+++ backend/scripts/start-prod.sh
@@ -3,7 +3,6 @@
 # Note: this script requires environment variables to be properly set beforehand, suggested to use systemd
 NODE_PATH=/home/ec2-user/.nvm/versions/node/v22.22.0/bin/node
 FLASK_PATH=/home/ec2-user/.local/bin/flask
-NGINX_PATH=/usr/sbin/nginx
-$NGINX_PATH -c daq.conf
 $NODE_PATH index.js &
 $FLASK_PATH --app microservices/microservices.py run -p 5000
+
